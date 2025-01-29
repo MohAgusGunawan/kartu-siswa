f4fd643817531ede9b1db7798e67e71b6cc1d4bd
agus <aguspamekasan464@gmail.com>
2025-01-29 10:10:28 +0700

    captcha

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -209,7 +209,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['token'])) {
     
         // Tambahkan event listener untuk menerima token dari Turnstile
         document.addEventListener("DOMContentLoaded", function () {
-            if (!document.getElementById('turnstile-widget').hasChildNodes()) {
+            const widgetContainer = document.getElementById('turnstile-widget');
+            if (widgetContainer && !widgetContainer.hasChildNodes()) {
+                console.log("Rendering Turnstile...");
                 turnstile.render('#turnstile-widget', {
                     sitekey: '0x4AAAAAAA6j75MpRvhSaHTH', // Ganti dengan Site Key Anda
                     callback: handleTurnstileCallback,
